ed4acae72d0a95dc456b9d3b6dfbacbef65590ae
Marcelo Martinho <martinho.guilherme13@hotmail.com>
2025-04-29 17:49:51 -0300

    insert into list and modify tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
 import "./App.css";
 import { useState } from "react";
 import { TaskType } from "./types/TaskTypes";
-import {v4 as uuidv4} from "uuid"
+import { v4 as uuidv4 } from "uuid";
 import { useLocalStorage } from "usehooks-ts";
+
+//imagem
+import emptyImg from "./assets/banerEmpty.svg";
 
 function App() {
   const [input, setInput] = useState("");
   const [tasks, setTasks] = useLocalStorage<TaskType[]>("task-list", []);
 
-  function handleKeyDown (event: React.KeyboardEvent<HTMLInputElement>) {
+  function handleKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
     if (input.length && event.key === "Enter") {
-        setTasks([...tasks, {id: uuidv4(), title: input, done: true }]);
-        setInput("");
-        console.log("tag add")
-      };
-      
+      setTasks([...tasks, { id: uuidv4(), title: input, done: false }]);
+      setInput("");
+      console.log("tag add");
     }
-  
+  }
+
+  function handleTaskToogle(id: string){
+    setTasks((prevState) => 
+      prevState.map((task) => 
+        task.id === id ? {...task, done: !task.done} : task))
+  }
 
   return (
     <div className="container-app">
@@ -38,18 +45,35 @@ function App() {
             className={`task-item ${t.done ? "task-item__done" : ""}`}
             key={t.id}
           >
-            <input type="checkbox" checked={t.done} readOnly />
+            <input 
+              type="checkbox" 
+              checked={t.done} 
+              readOnly 
+              onChange={() => handleTaskToogle(t.id)}
+            />
             {t.title}
           </li>
         ))}
+
+        {tasks.length === 0 && (
+          <div className="container-empty">
+            <img src={emptyImg} alt="img" />
+            <h3>Nenhuma tarefa encontrada!</h3>
+          </div>
+        )}
+
         <li className="content-tasks__actions">
-          <div><a href="#">Itens restantes</a></div>
+          <div>
+            <a href="#">Itens restantes</a>
+          </div>
           <div>
             <a href="#">Todas</a>
             <a href="#">Ativas</a>
             <a href="#">Completadas</a>
           </div>
-          <div><a href="#">Limpar Completadas</a></div>
+          <div>
+            <a href="#">Limpar Completadas</a>
+          </div>
         </li>
       </ul>
     </div>
